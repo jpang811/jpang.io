a68e727ab417de6be3198ee00515a7ac20521234
Jessica Pang <jessica_pang@asiamiles.com>
2017-01-17 15:05:42 +0800

    updated

--- js/map.js
+++ js/map.js
@@ -1,16 +1,92 @@
-function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
+var locations = [
+      ['Boston', 42.349626, -71.099536],
+      ['Hong Kong is where home is.', 22.282357, 114.157807],
+      ['London', 51.507911, -0.087749],
+      ['Hanoi', 21.032089, 105.847452],
+      ['Bangkok', 13.744752, 100.535061],
+      ['Seoul', 37.554551, 126.970670],
+      ['Roppongi', 35.662743, 139.731206],
+      ['Cancun', 21.201767, -86.805240],
+      ['Kuala Lumpur', 3.158024, 101.711578],
+      ['Bali', -8.795669, 115.230785]
+      
+  ];
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: myLatLng
+var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 3,
+      center: new google.maps.LatLng(32.523846, 16.134300),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+var infowindow = new google.maps.InfoWindow();
+
+var marker, i;
+
+for (i = 0; i < locations.length; i++) { 
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+    map: map
   });
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    return function() {
+      infowindow.setContent(locations[i][0]);
+      infowindow.open(map, marker);
+    }
+  })(marker, i));
 }
 
-marker.setMap(map);
+// This function will iterate over markersData array
+// creating markers with createMarker function
+function displayMarkers(){
+
+   // this variable sets the map bounds according to markers position
+   var bounds = new google.maps.LatLngBounds();
+   
+   // for loop traverses markersData array calling createMarker function for each marker 
+   for (var i = 0; i < markersData.length; i++){
+
+      var latlng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
+      var name = markersData[i].name;
+      var address1 = markersData[i].address1;
+      var address2 = markersData[i].address2;
+      var postalCode = markersData[i].postalCode;
+
+      createMarker(latlng, name, address1, address2, postalCode);
+
+      // marker position is added to bounds variable
+      bounds.extend(latlng);  
+   }
+
+   // Finally the bounds variable is used to set the map bounds
+   // with fitBounds() function
+   map.fitBounds(bounds);
+}
+
+// This function creates each marker and it sets their Info Window content
+function createMarker(latlng, name, address1, address2, postalCode){
+   var marker = new google.maps.Marker({
+      map: map,
+      position: latlng,
+      title: name
+   });
+
+   // This event expects a click on a marker
+   // When this event is fired the Info Window content is created
+   // and the Info Window is opened.
+   google.maps.event.addListener(marker, 'click', function() {
+      
+      // Creating the content to be inserted in the infowindow
+      var iwContent = '<div id="iw_container">' +
+            '<div class="iw_title">' + name + '</div>' +
+         '<div class="iw_content">' + address1 + '<br />' +
+         address2 + '<br />' +
+         postalCode + '</div></div>';
+      
+      // including content to the Info Window.
+      infoWindow.setContent(iwContent);
+
+      // opening the Info Window in the current map and at the current marker location.
+      infoWindow.open(map, marker);
+   });
+}
