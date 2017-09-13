878fa1483835ac6e8fef877d2ff569e93b5835ad
jpang811 <jpang0811@gmail.com>
2017-09-13 16:53:35 +0800

    travel.html 2.0

--- js/map.js
+++ js/map.js
@@ -1,3 +1,26 @@
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/*Location Data*/
 var locations = [
       ['#Boston is where great minds develop ;)', 42.349626, -71.099536],
       ['#HongKong is home', 22.282357, 114.157807],
@@ -14,7 +37,7 @@ var locations = [
       ['#Kolkata Two-week hotel internship at ITC Sonar Hotel', 22.544288, 88.398072],
       ['#Reykjavik Hearty lamb soup, geysirs and outdoor geothermal spa in the snow', 64.133663, -21.865861],
       ['#SanFrancisco Pursuing innovation at the Valley', 37.422042, -122.084058],
-      ['#Singapore Hong Kong\'s distant cousin', 1.287815, 103.866587],
+      ['#Singapore Hawker centers and tropical heat', 1.287815, 103.866587],
       ['#LasVegas The untold tales', 36.117905, -115.172966],
       ['#Arizona Drove through beautiful landscapes and Antelope Canyon', 36.862179, -111.374352],
       ['#Florida Where dreams come true and fun rides go', 28.385242, -81.563842],
@@ -26,121 +49,201 @@ var locations = [
       ['#Hawaii My favourite place in the world', 19.568144, -155.618030],
       ['#Cairo Young and fascinated by the mummies and desert', 30.047003, 31.235461],
       ['#IguazuFalls Bordering Brazil and Argentina', -25.690500, -54.440911],
-      ['#MachuPicchu Up in the mountains of Inca ruins and my first altitude sickness']
+      ['#MachuPicchu Up in the mountains of Inca ruins and my first altitude sickness'],
+      ['#Melbourne Artsy laneways, coffee culture and hipster vibes', -37.818125, 144.969064],
+      ['#Sydney The perfect balance between metropolitan and hipster', -33.867922, 151.211487],
+      ['#Auckland First gateway into New Zealand', -36.848457, 174.762180],
+      ['#Glasgow Cultural, dynamic and metropolitan ', 55.861239, -4.253990],
+      ['#Edinburgh Old Town hertiage, scottish whiskey and tales of Independence', 55.950474, -3.186297],
+      ['#Copenhagen Bikes, minimalism & all sorts of design wonders', 55.684065, 12.592988],
+      ['#Amsterdam Bites of happiness, red lights and that canal life ', 52.359106, 4.884022],
+      ['#Malmo Cardamon pastries, Fika (Swedish for tea time) and beautiful timber architecture', 55.604601, 13.002918],
+      ['#Prague A myriad of architectural gems and Bohemian beauty, followed by sunset walks on Charles Bridge', 50.086477, 14.411447],
+      ['#Cesky Krumlov Lunch by theWelcomed by the the most friendly faces', 48.812905, 14.313909],
+      ['#Salzburg Home to the Sound of Music and Mozart', 47.800205, 13.044633],
+      ['#Hallstatt Ferry rides, beautiful alpine views and has one of the oldest salt mines in the world', 47.563631, 13.636405],
+      ['#Vienna Coffee houses and fell for the classical tourist trap', 48.185216, 16.376528],
+      ['#Bratislava Nostalgic Old Town exploration and bronze statue hunting', 48.145116, 17.106877],
+      ['#Budapest Massive heat wave led by thermal baths, meat fares and ruin bars', 47.483752, 19.051643],
+      ['#Athens Imperfect yet so beautiful, Historical and so rawly preserved', 37.971541, 23.725771],
+      ['#Santorini Romantic sunsets and cave houses', 36.461557, 25.375281],
+      ['#Mykonos Volcanic beaches in red and black, beautiful alleys for boutique store shopping', 37.444530, 25.325594],
+      ['#Berlin Underground arts and raw creativity', 52.517377, 13.406855]
+
   ];
 
-var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 3,
-      center: new google.maps.LatLng(26.630547, 128.145678),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      styles: [
-         {
-           "featureType": "administrative",
-           "elementType": "labels.text.fill",
-           "stylers": [
-               {
-                   "color": "#444444"
-               }
-            ]
-         },
 
-         {
-              "featureType": "landscape",
-              "elementType": "all",
-              "stylers": [
-                  {
-                      "color": "#ededed"
-                  }
-              ]
-         },
+var map;
 
-         {
-              "featureType": "poi",
-              "elementType": "all",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-         },
-         
-         {
-              "featureType": "road",
-              "elementType": "all",
-              "stylers": [
-                  {
-                      "saturation": -100
-                  },
-                  {
-                      "lightness": 45
-                  }
-              ]
-          },
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 3,
+  center: {lat:26.630547, lng:128.145678},
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+  scrollwheel: false,
 
-         {
-              "featureType": "road.highway",
-              "elementType": "all",
-              "stylers": [
-                  {
-                      "visibility": "simplified"
-                  }
-              ]
-         },
-
-         {
-              "featureType": "road.arterial",
-              "elementType": "labels.icon",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-
-          {
-              "featureType": "transit",
-              "elementType": "all",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-
-          {
-              "featureType": "water",
-              "elementType": "all",
-              "stylers": [
-                  {
-                      "color": "#46bcec"
-                  },
-                  {
-                      "visibility": "on"
-                  }
-              ]
-          },
-
-          {
-              "featureType": "water",
-              "elementType": "geometry.fill",
-              "stylers": [
-                  {
-                      "color": "#74c1c1"
-                  }
-              ]
-          },
-
-          {
-              "featureType": "water",
-              "elementType": "labels.text",
-              "stylers": [
-                  {
-                      "color": "#ffffff"
-                  }
-              ]
-          }
-      ]
-});
+  styles: [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#91999c"
+            },
+            
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 80
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#404a4f"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2d393e"
+            }
+           
+        ]
+    }
+]
+  
+  });
 
 var infowindow = new google.maps.InfoWindow();
 
@@ -166,8 +269,8 @@ for (i = 0; i < locations.length; i++) {
 function createMarker(latlng, name, address1, address2, postalCode){
    var marker = new google.maps.Marker({
       map: map,
-      position: latlng,
-      title: name
+      title: name,
+      position: latlng
    });
 
    // This event expects a click on a marker
@@ -188,4 +291,7 @@ function createMarker(latlng, name, address1, address2, postalCode){
       // opening the Info Window in the current map and at the current marker location.
       infoWindow.open(map, marker);
    });
+}
+
+
 }
