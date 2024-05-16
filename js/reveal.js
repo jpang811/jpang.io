c0ec72746f10fb9717a844e2d2f3dca5e3d71693
jpang811 <jpang0811@gmail.com>
2024-05-16 16:35:44 +1000

    testing whether hover on mobile is disabled successfully

--- js/reveal.js
+++ js/reveal.js
@@ -6,7 +6,7 @@ function revealFunction(){
 
     window.sr = ScrollReveal({
         origin : 'top',
-        delay    : 400,
+        delay    : 300,
         opacity: 0,
         useDelay: 'once',
         mobile: true,
@@ -15,8 +15,25 @@ function revealFunction(){
         easing   : 'ease-in-out',
     })
 
-    sr.reveal('.img-responsive')
+    sr.reveal('.img-responsive');
+
+
+// need to learn how to sequence the reveal
+    window.sr = ScrollReveal({
+        origin : 'top',
+        delay    : 150,
+        opacity: 0,
+        useDelay: 'once',
+        mobile: true,
+        reset: false,
+        distance : '20px',
+        easing   : 'ease-in-out',
+    })
+
+    sr.reveal('.thumbnail-reveal')
 }
+
+
 
 window.addEventListener('load', () => {
 
